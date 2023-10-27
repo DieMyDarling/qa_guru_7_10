0be6e6528f040dc084b3953eda3da395f28918c9
Aleksey <sjim2ez4u@gmail.com>
2023-10-27 22:35:17 +0800

    user created

--- data/user.py
+++ data/user.py
@@ -1,4 +1,18 @@
 import dataclasses
+from enum import Enum
+from datetime import date
+
+
+class Gender(Enum):
+    male = 'Male'
+    female = 'Female'
+    other = 'Other'
+
+
+class Hobbies(Enum):
+    sports = 'Sports'
+    reading = 'Reading'
+    music = 'Music'
 
 
 @dataclasses.dataclass
@@ -6,14 +20,28 @@ class User:
     first_name: str
     last_name: str
     email: str
-    gender: str
+    gender: Gender
     phone_number: str
-    birth_day: str
-    birth_month: str
-    birth_year: str
+    birth_date: date
     subject: str
-    hobby: str
+    hobby: Hobbies
     picture: str
     address: str
     state: str
     city: str
+
+
+user = User(
+    'Alexey',
+    'Kokorev',
+    'test@gmail.ru',
+    Gender.male,
+    '1234567890',
+    date(15, 5, 1991),
+    'Maths',
+    Hobbies.sports,
+    'bat.png',
+    'Bali, Ubud, 1',
+    'NCR',
+    'Delhi'
+)
