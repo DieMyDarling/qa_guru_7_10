72b523363ab049d498cbb26561803f745c7ab7ee
Aleksey <sjim2ez4u@gmail.com>
2023-11-11 22:32:34 +0800

    Возвращены все return self в page object

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 def setup_browser(request):
     options = webdriver.ChromeOptions()
     options.browser_version = '100.0'
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-notifications')
